7394a09180abbcd50a4a390f51a5504396507260
Scoob <thatguyscoob1@gmail.com>
2024-03-26 23:32:44 +0000

    Code changes

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 choice = "0"
-def placeholder():
-  quit()
-def part1():
-  global choice
-  print(
-      "The date is March 1st, 2012, and you have decided to go explore a cave. You enter the cave and immediately encounter 2 different paths to take.\n"
-  )
-  choice = input("Do you go left (A), or go right (B)?")
+phase = 0
 
-  
-  match choice:
-    case "A":
-      choice = "0"
-      placeholder()
-    case "B":
-      choice = "0"
-      placeholder()
-    case _:
-      print("\nInvalid input. Try again.")
-  
+
+def placeholder():
+  print("PLACEHOLDER TEXT")
+  quit()
+
+
+while True:
+  if phase == 0:
+    print(
+        "The date is March 1st, 2012, and you have decided to go explore a cave. You enter the cave and immediately encounter 2 different paths to take."
+    )
+    choice = input("Do you go left, or go right?")
+    match choice:
+      case "left":
+        phase = 1
+        placeholder()
+      case "right":
+        print(2)
+        phase = 2
+        placeholder()
+      case _:
+        print("\nInvalid input. Try again.\n\n")
